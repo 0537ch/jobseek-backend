1a7aaceaafeb2085cfa189b44c90c2e04bf4b915
0537ch <abrar.ikramaputra@gmail.com>
2026-08-27 15:15:30 +0700

    CORS missing origin fix

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,6 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type, Accept, Authorization',
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 void bootstrap();
